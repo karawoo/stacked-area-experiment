5ec7a1a00bbf913131c6b8b35abdfe8837bf806b
Kara Woo <woo.kara@gmail.com>
2017-03-31 09:59:48 -0700

    Put training task on a separate page.
    Send data from that form on to the main task.

--- experiment_app/main.py
+++ experiment_app/main.py
@@ -10,21 +10,19 @@ def intro():
     # assignmentId etc., which we need to pass on to the next page in order to
     # capture and return it with the worker's answers.
     path = request.full_path
-    # Append /task at the beginning and remove initial / from path
+    # Create paths for training task and main task. Eventually we'll choose between these based on whether we've seen the user before?
     task_path = "/task" + path[1:]
-    return render_template("intro.html", task_path = task_path)
+    train_path = "/training" + path[1:]
+    return render_template("intro.html", path = train_path)  # Hard-coded to train path for now
 
 # Serve chart images
 @app.route("/charts/<path:path>")
 def serve_charts(path):
     return send_from_directory("static/images", path)
 
-# Show a task with a (for now) hard-coded image
-@app.route("/task")
-def serve_task():
-    img = "stacked_area_decreasing_0.825_A.png"
-    imgpath = "charts/" + img
-
+# Show training task
+@app.route("/training")
+def serve_training():
     train_img = "stacked_area_TEST_1.png"
     train_imgpath = "charts/" + train_img
 
@@ -32,10 +30,30 @@ def serve_task():
     hit_id = request.args["hitId"]
     assignment_id = request.args["assignmentId"]
     worker_id = request.args["workerId"]
+
+    return render_template("training.html", train_image_url = train_imgpath,
+                           hit_id = hit_id, assignment_id = assignment_id,
+                           worker_id = worker_id)
+
+# Show a task with a (for now) hard-coded image
+@app.route("/task", methods = ["POST"])
+def serve_task():
+    img = "stacked_area_decreasing_0.825_A.png"
+    imgpath = "charts/" + img
+
+    if request.method == "POST":
+        # Answers to Q1 and Q2
+        Q1 = request.form["Q1"]
+        Q2 = request.form["Q2"]
+        
+        # Get workerId etc. from request
+        hit_id = request.form["hitId"]
+        assignment_id = request.form["assignmentId"]
+        worker_id = request.form["workerId"]
     
-    return render_template("task.html", train_image_url = train_imgpath,
-                           image_url = imgpath, hit_id = hit_id,
-                           assignment_id = assignment_id, worker_id = worker_id)
+    return render_template("task.html", image_url = imgpath, Q1 = Q1, Q2 = Q2,
+                           hit_id = hit_id, assignment_id = assignment_id,
+                           worker_id = worker_id)
 
 if __name__ == '__main__':
     app.run()
